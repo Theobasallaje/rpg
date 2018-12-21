dbcefb0b5ebf28f53404f9bfdb10362b29a40ec0
Theo <theobasallaje@gmail.com>
2018-12-21 17:55:44 -0600

    attack logic - complete v1

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,28 @@
 //object that holds the each character objects with their properties that lists out all of the players and their images
-
-
 var superMan =  {
     name: "Superman",
     image: "../rpg/assets/images/superman.png", 
-    hp: 100, 
+    hp: 250, 
     damage: 0, 
-    attackPowewr: 0,
-    counterAttackPower: 0
+    attackPower: 50,
+    counterAttackPower: 50
 }
 var bizarro =  {
     name: "Bizzaro",
     image: "../rpg/assets/images/bizarro.png", 
-    hp: 100, 
+    hp: 200, 
     damage: 0, 
-    attackPowewr: 0,
-    counterAttackPower: 0
+    attackPower: 50,
+    counterAttackPower: 50
 }
 
 var theFlash = {
     name: "The Flash",
     image: "../rpg/assets/images/flash.jpg", 
-    hp: 100, 
+    hp: 150, 
     damage: 0, 
-    attackPowewr: 0,
-    counterAttackPower: 0
+    attackPower: 25,
+    counterAttackPower: 25
 }
 
 var reverseFlash = {
@@ -32,39 +30,34 @@ var reverseFlash = {
     image: "../rpg/assets/images/reverseflash.jpg", 
     hp: 100, 
     damage: 0, 
-    attackPowewr: 0,
-    counterAttackPower: 0
+    attackPower: 25,
+    counterAttackPower: 25
 }
 
+//arrays
 var characters = [superMan, bizarro, theFlash, reverseFlash]
-//arryas
- 
 
- //global variables 
- 
-
- // Create variables that hold references to the places in the HTML where we want to display things.
- var directionsText = $("#directions");
+ // Create global variables that hold references to the places in the HTML where we want to display things.
+ var result = $("#result");
  var player = $("#player");
  var characterDisplay = $("#characters");
  var superman = $("#superMan");
  var bizarro = $("#bizarro");
- var bizarro = $("#reverseFlash");
  var enemies = $("#enemies");
  var fight = $("#fight");
  var defend = $("#defend");
  var defender = $("#defender");
+ var defender2 = $("#defender2");
  var playerDamage = $("#playerDamage");
  var defenderDamage = $("#defenderDamage");
 
 
  var playerOne;
  var enemy;
+//  var chosenCharacter;
 
  //functions
-//  function fight(){
-//     player.html("<img src=" + characters.heros.superMan.image + ">");       
-//  }
+
 function populateCharacters(display){
     display.empty();
     console.log("In populate characters")
@@ -112,9 +105,33 @@ function populateCharacters(display){
     });
 }
 
-
-
  $(document).ready(function() {
+    $("#button").click(function(){
+        playerOne.damage = playerOne.damage + enemy.counterAttackPower;
+        playerOne.hp = playerOne.hp - enemy.counterAttackPower;
+        enemy.damage = enemy.damage + playerOne.attackPower;
+        enemy.hp = enemy.hp - playerOne.attackPower;
+
+        if (playerOne.hp === 0){
+            result.text("You have been defeated! GAME OVER...");    
+        } 
+        else if (enemy.hp === 0){
+            result.text("You saved the day!");
+        }
+    
+        else if (!(playerOne.hp === 0 && enemy.hp === 0)){
+            // console.log(playerOne.hp);
+            // console.log(playerOne.damage);
+            console.log(playerOne.attackPower);
+            
+            // console.log(enemy.hp);
+            defender.text(enemy.name);
+            defender2.text(enemy.name);
+            playerDamage.text(playerOne.damage);
+            defenderDamage.text(enemy.damage);
+        }
+    });
+    
      populateCharacters(characterDisplay);
     // function selectCharacter(){
     //     superman.click(function() {
